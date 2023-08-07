5f176197e392a69b269a07d2881e90dd3039339f
Amir <ahossein960@gmail.com>
2023-08-07 12:00:41 +0330

    commit finalization Title.js & Title.css

--- src/Components/Home/Title/Title.js
+++ src/Components/Home/Title/Title.js
@@ -1,15 +1,14 @@
 import "./Title.css"
 
 
-
 function Title(){
     return(
         <>
-            <div style={{display:"flex" , width:"100%" , height:"100px"}} className="Container">
+            <div style={{display:"flex" , width:"100%" , height:"100%"}} className="Container">
                 <div className="Title">
 
                 </div>
-                <p style={{marginLeft:"5px" , color:"red"}}>Today's </p>
+                <p>Today's </p>
             </div>
         </>
     )
