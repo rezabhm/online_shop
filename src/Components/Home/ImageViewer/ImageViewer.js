c7eca2bbfdf392cba5ee5c982dac83d8e17a1b72
emad <arabanemad@gmail.com>
2023-08-09 15:49:44 +0330

    ImageViewer Compelet v(2)

--- src/Components/Home/ImageViewer/ImageViewer.js
+++ src/Components/Home/ImageViewer/ImageViewer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Carousel from "react-material-ui-carousel";
 import Item from "./Item";
 import { Box } from "@material-ui/core";
