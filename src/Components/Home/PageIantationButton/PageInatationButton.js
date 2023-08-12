c92e47f014328626ccd74cf80a5b5b02e41af1f1
emad <arabanemad@gmail.com>
2023-08-12 14:47:29 +0330

    PageIantationButton Update

--- src/Components/Home/PageIantationButton/PageInatationButton.js
+++ src/Components/Home/PageIantationButton/PageInatationButton.js
@@ -3,20 +3,19 @@ import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
 import ArrowForwardOutlinedIcon from "@mui/icons-material/ArrowForwardOutlined";
-const PageInatationButton = () => {
+const PageInatationButton = (props) => {
   return (
     <>
       <Box sx={{ "& > :not(style)": { m: 1 } }}>
-        <Fab onClick={()=>{"previus page"}} color="##e8eaf6" aria-label="add">
+        <Fab onClick={props.NextClick} color="##e8eaf6" aria-label="add">
           <ArrowBackOutlinedIcon />
         </Fab>
-        <Fab onClick={()=>{"next page"}} color="##e8eaf6" aria-label="edit">
+        <Fab onClick={props.prevClick} color="##e8eaf6" aria-label="edit">
           <ArrowForwardOutlinedIcon />
         </Fab>
       </Box>
     </>
   );
 };
-
 
 export default PageInatationButton;
