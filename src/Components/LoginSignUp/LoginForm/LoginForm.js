65ee117dd7ccc91dc3c47523646cb0dcd37e3f34
Amir <ahossein960@gmail.com>
2023-08-12 16:39:15 +0330

    commit finalization LoginForm.js

---
+++ src/Components/LoginSignUp/LoginForm/LoginForm.js
@@ -0,0 +1,66 @@
+import {
+    FormControl,
+    Grid,
+    IconButton, Input, InputAdornment,
+    InputLabel, OutlinedInput,
+    TextField,
+} from "@mui/material";
+import * as React from "react";
+import {Visibility, VisibilityOff} from "@material-ui/icons";
+
+export default function LoginForm() {
+
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    }
+
+
+
+        return (
+            <>
+
+                <Grid container xs={12} spacing={2}>
+
+                    <Grid item xs={12}>
+                        <TextField
+                            onChange={()=> ("Done PassWord")}
+                            id="standard-basic"
+                            label="UserName"
+                            variant="standard"
+                            sx={{width: "100%"}}
+                        />
+                    </Grid>
+
+                    <Grid item xs={12}>
+
+                        <FormControl sx={{ m: 1, width: '100%' }} variant="standard">
+                            <InputLabel onChange={()=> ("Done PassWord")} htmlFor="standard-adornment-password">PassWord</InputLabel>
+                            <Input
+                                id="standard-adornment-password"
+                                type={showPassword ? 'text' : 'password'}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                            />
+                        </FormControl>
+
+                    </Grid>
+                </Grid>
+
+
+            </>
+        )
+
+}
