dfe9028064cac325824a6f682dd227eb149fdc7a
Reza <rezafafaa78@gmail.com>
2023-08-20 03:30:08 +0330

    index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import CategoryList from "./Components/Home/CategoryList/CategoryList";
 import NavBar from "./Components/Home/Navbar/NavBar";
 import Home from "./Pages/Home";
 import ImageViewer from "./Components/Home/ImageViewer/ImageViewer";
+
+import ProductImage from "./Components/ProductPage/ProductImage/ProductImage";
 import PageInatationButton from "./Components/Home/PageIantationButton/PageInatationButton";
-import { IconBtn } from "./Components/Home/PageIantationButton/iconData";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(
+    
+<ProductImage/>
+)
