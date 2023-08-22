15cfbf2145ed5b543535bf595e139619d3611c36
emad <arabanemad@gmail.com>
2023-08-22 12:18:54 +0330

    create productDescription

---
+++ src/Components/ProductPage/Product description/ProductDescription.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+const ProductDescription = () => {
+  return (
+    <Typography>
+      PlayStation 5 Controller Skin High quality vinyl with air channel adhesive
+      for easy bubble free install & mess free removal Pressure sensitive.
+    </Typography>
+  );
+};
+
+export default ProductDescription;
