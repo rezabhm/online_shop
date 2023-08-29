15ffd24a00edbaf3ccf88a0163b96d7c75c46945
emad <arabanemad@gmail.com>
2023-08-29 11:43:39 +0330

    Update

--- src/Components/Home/Product/Product.js
+++ src/Components/Home/Product/Product.js
@@ -7,40 +7,44 @@ import {
   CardMedia,
   Stack,
   Typography,
-  Box
+  Box,
 } from "@mui/material";
 import Rating from "@mui/material/Rating";
 
 function Product(props) {
   return (
-    <>
+    <Box>
       <Card
         sx={{
-          width: "90%",
-          height: {xs:"80vh" , md:"60vh"},
-          
+          maxWidth: "300px",
+          maxHeight: "500px",
+
           backgroundColor: "",
         }}
       >
-        <CardMedia
+        {/* <CardMedia
           image={props.image}
           title={"product "}
           sx={{
-          
-            height: {xs:"65%",md:"65%"},
+            height: "100%",
             width: "100%",
             marginBottom: "10px",
             // border:'1px solid black'
-      
           }}
-        />
-
+        /> */}
+        <Box>
+          <img
+            style={{ height: "100%", width: "100%", marginBottom: "10px" }}
+            src={props.image}
+            alt=""
+          />
+        </Box>
         <CardContent>
           <Button
             sx={{
               backgroundColor: "black",
               color: "#FFFFFF",
-              
+
               width: "100%",
               "&:hover": { backgroundColor: "red" },
               marginBottom: "10px",
@@ -51,53 +55,43 @@ function Product(props) {
           <Typography variant={"h5"}>{props.title}</Typography>
         </CardContent>
 
-        <CardActions sx={{width:"100%"}}>
-            <Stack>
-          <Box>
-            <Typography
-              variant={"h6"}
+        <CardActions sx={{ width: "100%" }}>
+          <Stack>
+            <Box>
+              <Typography
+                variant={"h6"}
+                sx={{
+                  color: "red",
+                }}
+              >
+                ${props.price}
+              </Typography>
+            </Box>
+
+            <Box
               sx={{
-                color: "red",
-               
+                display: "flex",
               }}
             >
-              ${props.price}
-            </Typography>
-          </Box>
-
-    
-
-         
-
-
-                <Box
-            sx={{
-              
-              display:"flex"
-            }}
-          >
-          <Box sx={{marginRight:"8px"}}>
-            <Rating
-              name={"half-rating"}
-              defaultValue={props.stars}
-              precision={0.5}
-              readOnly
-            />
+              <Box sx={{ marginRight: "8px" }}>
+                <Rating
+                  name={"half-rating"}
+                  defaultValue={props.stars}
+                  precision={0.5}
+                  readOnly
+                />
+              </Box>
+              <Box>
+                {" "}
+                <Typography variant={"h6"} sx={{}}>
+                  {props.rateNumber}
+                </Typography>
+              </Box>
             </Box>
-            <Box> <Typography
-            variant={"h6"}
-            sx={{
-            
-            }}
-          >
-            {props.rateNumber}
-          </Typography></Box>
-          </Box>
           </Stack>
         </CardActions>
-      
       </Card>
-    </>
+    </Box>
   );
 }
 export default Product;
--- src/Pages/ProducPage.jsx
+++ src/Pages/ProducPage.jsx
@@ -1,4 +1,4 @@
-import { Grid,styled } from "@mui/material";
+import { Grid, styled } from "@mui/material";
 import React from "react";
 import NavBar from "../Components/Home/Navbar/NavBar";
 import ProductImage from "../Components/ProductPage/ProductImage/ProductImage";
@@ -17,16 +17,16 @@ import Title from "../Components/Home/Title/Title";
 import Product from "../Components/Home/Product/Product";
 import Footer from "../Components/Home/Footer/Footer";
 import { CarCrash, CatchingPokemon } from "@mui/icons-material";
-const Maingrid=styled("grid")(({theme})=>({
+const Maingrid = styled("grid")(({ theme }) => ({
   height: "50vh",
-  width:"25%",
-  
-  [theme.breakpoints.down("sm")]:{
-    height:'70vh',
-    width:"100vw",
-    marginBottom:"90px"
-  }
-}))
+  width: "25%",
+
+  [theme.breakpoints.down("sm")]: {
+    height: "70vh",
+    width: "100vw",
+    marginBottom: "90px",
+  },
+}));
 export const ProducPage = () => {
   return (
     <>
@@ -42,14 +42,7 @@ export const ProducPage = () => {
             />
           </Grid>
 
-          <Grid
-            xs={12}
-            sm={12}
-            md={5}
-            sx={{
-            
-            }}
-          >
+          <Grid xs={12} sm={12} md={5} sx={{}}>
             <Box sx={{ marginBottom: "10px" }}>
               <ProductHeader />
             </Box>
@@ -93,73 +86,68 @@ export const ProducPage = () => {
         </Grid>
 
         <Grid
-        container
+          container
           sx={{
             marginTop: "10%",
             marginBottom: "10%",
-        
+
             display: "flex",
             marginTop: "6%",
             justifyContent: "space-between",
             width: "100%",
 
-            height:{xs:'100%'},
-            
-           
-           
+            height: { xs: "100%" },
           }}
         >
           {/* <Title title={"Related Item"} /> */}
 
-         
-            <Maingrid xs={12} sm={12} md={3} xl={3} >
-              <Product
-                image={
-                  "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-finish-select-202207-6-1inch-blue?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1656712888128"
-                }
-                title={"Abzar"}
-                price={100}
-                star={4}
-                rateNumber={25}
-              />
-            </Maingrid>
+          <Maingrid xs={12} sm={12} md={3} xl={3}>
+            <Product
+              image={
+                "https://www.technolife.ir/image/color_image_TLP-24069_22a148_67f07ac9-7e8b-4fba-be60-92657a17a611.png"
+              }
+              title={"Abzar"}
+              price={100}
+              star={4}
+              rateNumber={25}
+            />
+          </Maingrid>
 
-            <Maingrid xs={12} sm={12} md={3} xl={3}  >
-              <Product
-                image={
-                  "https://www.technolife.ir/image/color_image_TLP-24069_22a148_67f07ac9-7e8b-4fba-be60-92657a17a611.png"
-                }
-                title={"Mobile"}
-                price={250}
-                star={3.5}
-                rateNumber={50}
-              />
-            </Maingrid>
+          <Maingrid xs={12} sm={12} md={3} xl={3}>
+            <Product
+              image={
+                "https://www.technolife.ir/image/color_image_TLP-24069_22a148_67f07ac9-7e8b-4fba-be60-92657a17a611.png"
+              }
+              title={"Mobile"}
+              price={250}
+              star={3.5}
+              rateNumber={50}
+            />
+          </Maingrid>
 
-            <Maingrid xs={12} sm={12} md={3} xl={3}  >
-              <Product
-                image={
-                  "https://parhantech.com/wp-content/uploads/2022/12/12PROMAX-BLACK.jpg"
-                }
-                title={"Tala"}
-                price={1500}
-                star={5}
-                rateNumber={250}
-              />
-            </Maingrid>
+          <Maingrid xs={12} sm={12} md={3} xl={3}>
+            <Product
+              image={
+                "https://www.technolife.ir/image/color_image_TLP-24069_22a148_67f07ac9-7e8b-4fba-be60-92657a17a611.png"
+              }
+              title={"Tala"}
+              price={1500}
+              star={5}
+              rateNumber={250}
+            />
+          </Maingrid>
 
-            <Maingrid xs={12} sm={12} md={3} xl={3} >
-              <Product
-                image={
-                  "https://oj724.com/wp-content/uploads/2022/12/xskx-1.jpg"
-                }
-                title={"chador"}
-                price={75}
-                star={1}
-                rateNumber={10}
-              />
-            </Maingrid>
-          
+          <Maingrid xs={12} sm={12} md={3} xl={3}>
+            <Product
+              image={
+                "https://www.technolife.ir/image/color_image_TLP-24069_22a148_67f07ac9-7e8b-4fba-be60-92657a17a611.png"
+              }
+              title={"chador"}
+              price={75}
+              star={1}
+              rateNumber={10}
+            />
+          </Maingrid>
         </Grid>
       </Container>
 
