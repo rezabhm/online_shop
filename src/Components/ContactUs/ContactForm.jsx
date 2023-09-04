805297b2ce54b5c0329830c2e9371eb879a26b07
emad <arabanemad@gmail.com>
2023-09-04 17:28:24 +0330

    Create ContactUS

---
+++ src/Components/ContactUs/ContactForm.jsx
@@ -0,0 +1,64 @@
+import { Box, Button, Grid, TextField } from "@mui/material";
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <Grid container>
+      {" "}
+      <Grid xs={12} sm={12} md={4} sx={{ p: 1 }}>
+        <TextField
+          placeholder="Your Name *"
+          variant="filled"
+          sx={{
+            width: { sm: "100%", xs: "100%" },
+          }}
+        />
+      </Grid>
+      <Grid xs={12} sm={12} md={4} sx={{ p: 1 }}>
+        <TextField
+          placeholder="Your Email *"
+          variant="filled"
+          sx={{
+            width: { sm: "100%", xs: "100%" },
+          }}
+        />
+      </Grid>
+      <Grid xs={12} sm={12} md={4} sx={{ p: 1 }}>
+        <TextField
+          placeholder="Your Phone *"
+          variant="filled"
+          sx={{
+            width: { sm: "100%", xs: "100%" },
+          }}
+        />
+      </Grid>
+      <Grid xs={12} sm={12} md={12}>
+        <TextField
+          placeholder="Your Massage *"
+          variant="filled"
+          sx={{
+            width: "97.7%",
+            mt: 2,
+            ml: 1,
+            "& .MuiInputBase-root": {
+              height: 200,
+            },
+            "& .MuiInputBase-input": {
+              position: "absolute",
+              top: "0",
+            },
+          }}
+        />
+        <Button
+          variant="contained"
+          color="error"
+          sx={{ float: "right", mr: 1, mt: 2, p: 2 }}
+        >
+          Send Massage
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default ContactForm;
