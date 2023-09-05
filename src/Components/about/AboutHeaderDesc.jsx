7d5c59ab90dca476a6743be49841f3222c45a1dc
emad <arabanemad@gmail.com>
2023-09-05 18:03:53 +0330

    Update AboutHeaderDesc & AboutImg

--- src/Components/about/AboutHeaderDesc.jsx
+++ src/Components/about/AboutHeaderDesc.jsx
@@ -1,23 +1,18 @@
-import React from 'react'
-import ProductDescription from '../ProductPage/Product description/ProductDescription'
-import ProductHeader from '../ProductPage/Product Header/ProductHeader'
-import { Box } from '@mui/material'
+import React from "react";
+import ProductDescription from "../ProductPage/Product description/ProductDescription";
+import ProductHeader from "../ProductPage/Product Header/ProductHeader";
+import { Box } from "@mui/material";
 const AboutHeaderDesc = (props) => {
-
   return (
     <>
-    <Box sx={{width:"525px" , height:'373px'}}>
-     <Box sx={{mb:"5%"}}>
-    <ProductHeader 
-    title={props.title}
-    />
-    </Box>
-    <ProductDescription 
-    text={props.text}
-    />
-    </Box>
+      <Box sx={{ width: "100%", height: "373px", textAlign: "justify" }}>
+        <Box sx={{ mb: "5%" }}>
+          <ProductHeader title={props.title} />
+        </Box>
+        <ProductDescription text={props.text} />
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default AboutHeaderDesc
+export default AboutHeaderDesc;
