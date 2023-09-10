3879380924c2808d3fb24395fbd3b7a627c07028
emad <arabanemad@gmail.com>
2023-09-10 11:48:35 +0330

    Index Update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CategoryList from "./Components/Home/CategoryList/CategoryList";
-import NavBar from "./Components/Home/Navbar/NavBar";
 import Home from "./Pages/Home";
-import ImageViewer from "./Components/Home/ImageViewer/ImageViewer";
+import "./public.css";
 
-import ProductImage from "./Components/ProductPage/ProductImage/ProductImage";
-import PageInatationButton from "./Components/Home/PageIantationButton/PageInatationButton";
-import OurProducts from "./Components/Home/OurProducts/OurProducts";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    
-<>
-    <Home/>
-
-    </>
-)
+  <>
+    <Home />
+  </>
+);
