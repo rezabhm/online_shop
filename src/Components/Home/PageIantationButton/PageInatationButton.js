74d7d0691d5a360e9e1288ad41097f82415dc816
emad <arabanemad@gmail.com>
2023-09-11 15:42:51 +0330

    Merge branch 'master' of https://github.com/rezabhm/online_shop

--- src/Components/Home/PageIantationButton/PageInatationButton.js
+++ src/Components/Home/PageIantationButton/PageInatationButton.js
@@ -3,30 +3,37 @@ import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
 import ArrowForwardOutlinedIcon from "@mui/icons-material/ArrowForwardOutlined";
+import { Grid } from "@mui/material";
 const PageInatationButton = (props) => {
-    return (
-        <>
-            <Box sx={{ "& > :not(style)": { m: 1,
-
-
-                },
-
-                alignItems:'right',
-                justifyContent:'right',
-                display:'flex',
-                right:'0px',
-                mr:'0px'
-
-            }}>
-                <Fab onClick={props.NextClick} color="##e8eaf6" aria-label="add">
-                    <ArrowBackOutlinedIcon />
-                </Fab>
-                <Fab onClick={props.prevClick} color="##e8eaf6" aria-label="edit">
-                    <ArrowForwardOutlinedIcon />
-                </Fab>
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Grid
+        container
+        sx={{ mr: 8, mb: 3, display: { xs: "none", md: "flex" } }}
+      >
+        <Grid xs={12} md={6} sx={{ width: "100%" }}>
+          <Fab
+            onClick={props.NextClick}
+            sx={{}}
+            color="##e8eaf6"
+            aria-label="add"
+          >
+            <ArrowBackOutlinedIcon />
+          </Fab>
+        </Grid>
+        <Grid xs={12} md={6} sx={{ width: "100%" }}>
+          <Fab
+            onClick={props.prevClick}
+            sx={{}}
+            color="##e8eaf6"
+            aria-label="edit"
+          >
+            <ArrowForwardOutlinedIcon />
+          </Fab>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default PageInatationButton;
